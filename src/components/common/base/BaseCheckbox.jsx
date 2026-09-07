760b2ff2fdada2f62138499783187bc8a8b1a2f4
gyuwon <moongyu0413@gmail.com>
2026-09-07 15:10:43 +0900

    feat: BaseCheckbox 컴포넌트 구현

--- src/components/common/base/BaseCheckbox.jsx
+++ src/components/common/base/BaseCheckbox.jsx
@@ -1,8 +1,23 @@
-export default function BaseCheckbox({ checked, onChange, label }) {
-  return (
-    <label className="base-checkbox">
-      <input type="checkbox" checked={checked} onChange={onChange} />
-      {label}
-    </label>
-  );
+function BaseCheckbox({ checked, onClick }) {
+    return (
+        <div
+            onClick={onClick}
+            style={{
+                width: "24px",
+                height: "24px",
+                border: "1px solid #999",
+                borderRadius: "6px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                cursor: "pointer",
+                backgroundColor: checked ? "#4CAF50" : "white",
+                color: "white",
+            }}
+        >
+            {checked ? "✓" : ""}
+        </div>
+    );
 }
+
+export default BaseCheckbox;
